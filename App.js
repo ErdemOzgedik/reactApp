8127e62853b5c88656b2c54e5d787b1798916b16
Erdem Özgedik <erdemozgedik@outlook.com>
2018-11-11 16:37:30 +0300

    first inputText added to our app

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+
+  state = {
+    placeName: ""
+  };
+
+  placeNameChangedHandler = value => {
+    this.setState({
+      placeName: value
+    })
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text>My first React Native app.</Text>
+        <TextInput
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+          style={{ width: 300, borderColor: "black", borderWidth: 1 }}
+          placeholder="Please enter where you are live !!!"
+        />
+        <Text>You are live in {this.state.placeName}!</Text>
       </View>
+
     );
   }
 }
